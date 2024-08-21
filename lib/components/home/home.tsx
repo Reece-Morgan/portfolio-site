6c2113fc3f3cf679d7b3e94bbd05ea91e3d6d4da
Reece <reecemorgandev@gmail.com>
2024-08-21 16:56:30 +0100

    added welcome section

--- lib/components/home/home.tsx
+++ lib/components/home/home.tsx
@@ -1,9 +1,10 @@
+import { Welcome } from '../welcome/welcome';
 import styles from './home.module.css';
 
 export const Home = () => {
     return (
         <main className={styles.main}>
-            <h1>Portfolio</h1>
+            <Welcome />
         </main>
     )
 }
