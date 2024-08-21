b517159005386608887c1cc4b06601040f2aba5c
Reece <reecemorgandev@gmail.com>
2024-08-21 16:33:26 +0100

    created home component

---
+++ lib/components/home/home.tsx
@@ -0,0 +1,9 @@
+import styles from './home.module.css';
+
+export const Home = () => {
+    return (
+        <main className={styles.main}>
+            <h1>Portfolio</h1>
+        </main>
+    )
+}
