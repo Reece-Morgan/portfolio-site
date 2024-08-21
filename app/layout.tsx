39915a267e4d46b2fc81d86d3031cf3fe5d62041
Reece <reecemorgandev@gmail.com>
2024-08-21 16:56:04 +0100

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import "./globals.css";
+import "./global.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Portfolio | Reece Morgan",
-  description: "A Portfolio put together by Reece morgan to showcase his frontend development work",
+  description:
+    "A Portfolio put together by Reece morgan to showcase his frontend development work",
 };
 
 export default function RootLayout({
